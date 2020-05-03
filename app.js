4f8f24c645153f04b2df46238cc7ee1b9bcde915
Jose Ramon Vargas <doombos@me.com>
2020-05-03 18:58:46 -0500

    .

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ cron.schedule("*/30 * * * *", function() {
 });
 
 //SSA File processing
-cron.schedule("*/1 * * * *", function() {
+cron.schedule("*/35 * * * *", function() {
     //validate the other file
     var d = new Date();
     console.log("processing SSA covid19 files if needed every 35 minutes");
